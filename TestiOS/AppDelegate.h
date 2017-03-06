f196b5d200ba1ffafca458f317fade6db2623335
Keith Hon <keithhon@cherrypicks.com>
2017-03-06 10:52:26 +0800

    Initial commit

---
+++ TestiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestiOS
+//
+//  Created by admin on 6/3/2017.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestiOS
+//
+//  Created by admin on 6/3/2017.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
